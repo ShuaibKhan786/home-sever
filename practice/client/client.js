4aa4955105855c06535a64cd04d8f9b177f21b40
ShuaibKhan786 <skshuaibkhan876@gmail.com>
2023-05-01 14:03:53 +0530

    submodule problem resolve

---
+++ practice/client/client.js
@@ -0,0 +1,26 @@
+
+// const str = "I am Shuiab";
+
+// const encoder = new TextEncoder();
+// const uint8arrayEncoded = encoder.encode(str);
+
+// console.log(uint8arrayEncoded);
+
+// const decoder = new TextDecoder("utf-8");
+// const uint8arrayDecoded = decoder.decode(uint8array)
+// console.log(uint8arrayDecoded);
+
+
+// blob
+
+    // true
+    {
+        var x = 12;
+    }
+    console.log(x)
+
+    // true
+    {
+        let y = 10;
+    }
+    console.log(y)
